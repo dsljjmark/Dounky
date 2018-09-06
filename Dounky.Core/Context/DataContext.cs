8530da374af3e998e60be599dc4a2e272a99991a
Panupong Kittisupaluk <Panupong@indigy.com>
2018-09-06 10:19:43 +0700

    add context and services and appsetting

---
+++ Dounky.Core/Context/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Dounky.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dounky.Core.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
