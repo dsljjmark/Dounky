c5272efe61a1925019ed2580b383411cab0476ac
Panupong Kittisupaluk <Panupong@indigy.com>
2018-09-06 10:01:33 +0700

    add automapper and automapperprofile

--- Dounky.API/Startup.cs
+++ Dounky.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Dounky.API.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,8 +28,10 @@ namespace Dounky.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            //swagger
             services.AddSwaggerDocumentation();
+            //autoMapper
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +40,7 @@ namespace Dounky.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //swagger
                 app.UseSwaggerDocumentation();
             }
 
---
+++ Dounky.Core/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dounky.Core.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //CreateMap<User, UserDto>();
+            //CreateMap<UserDto, User>();
+        }
+    }
+}
